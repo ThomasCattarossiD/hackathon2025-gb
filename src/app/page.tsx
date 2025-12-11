f0d36829abdc559755cf30f3661d6620a752004b
Ylan Lodi <ylan.lodi@gmail.com>
2025-12-11 19:19:30 +0100

    ajout tts, ajustement interface et bouton retour bas de page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import ChatInterface from "@/components/chat/ChatInterface";
 
 export default function ChatPage() {
   return (
-    <main className="bg-gray-100">
+    <main className="h-[100dvh] w-full bg-gray-100 flex items-center justify-center overflow-hidden">
       <ChatInterface />
     </main>
   );
